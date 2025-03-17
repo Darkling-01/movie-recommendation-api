c37f4264f5638db9d4ef04b0ec07e7b9c4a793b1
Alejandro Barragan <57509594+Darkling-01@users.noreply.github.com>
2025-03-16 21:55:42 -0700

    user input was able to work

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from app.routes.movies import router as movie_router
+from pydantic import BaseModel
 
 app = FastAPI()
 
 app.include_router(movie_router)
+
+
+# asking for user input
+class PreferenceInput(BaseModel):
+    
+    media_type: str
+    original_name: str
+
+@app.post("/preferences/")
+async def set_preference(preference: PreferenceInput):
+    return {"message": f"Media type is {preference.media_type} with actor {preference.original_name}"}
 

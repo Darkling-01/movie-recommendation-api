78a7f8b02e3fbabd13485f123ce46a4fb48ab978
Alejandro Barragan <57509594+Darkling-01@users.noreply.github.com>
2025-03-18 21:26:48 -0700

    implemented function to search for actor name
    There is a bug displaying the information regarding the actor's information.

--- app/main.py
+++ app/main.py
@@ -9,11 +9,18 @@ app.include_router(movie_router)
 
 # asking for user input
 class PreferenceInput(BaseModel):
-    
+
     media_type: str
     original_name: str
 
 @app.post("/preferences/")
 async def set_preference(preference: PreferenceInput):
-    return {"message": f"Media type is {preference.media_type} with actor {preference.original_name}"}
+    media_type = preference.media_type
+
+    if media_type == "movie" or media_type == "Movie":
+        return {"message": f"Type: {preference.media_type} with Actor: {preference.original_name}"}
+    elif media_type == "tv" or media_type == "TV":
+        return {"message": f"Type: {preference.media_type} with Actor: {preference.original_name}"}
+    else:
+        return {"message": "Invalid Response."}
 
--- app/routes/movies.py
+++ app/routes/movies.py
@@ -6,12 +6,34 @@ router = APIRouter()
 # @ are decorator functions that modify or enhance the behavior of 
 # other functions without changing code directly
 
-@router.get("/movies/recommendations")# route handler
+@router.get("/media/allresults")   # route handler
 
 # async functions run concurrently and are ideal for API call
 async def movie_recommendations():
-    movie_data = await get_movie_recommendations()
-    if "movies" in movie_data:
-        return {"movies":movie_data["movies"]}
+    media_data = await get_movie_recommendations()
+    if "movie" in media_data:
+        return {"movie":media_datail}
     else:
-        return movie_data
+        return media_data
+
+# Receiving user's preferences by using GET to searh through collection
+@router.get("/media/filtering")
+
+async def filtering_user_input(actor_name: str=None):
+    media_data = await get_movie_recommendations()
+    # Apply filtering logic
+    filtered_data = []
+
+    # filter data based on media type and actor
+    for media in media_data:
+       try:
+           if isinstance(media, dict):
+               if actor_name and media.get("original_name") == actor_name:
+                   filtered_data.append(media)
+           else:
+               print(f"Skipping non-dictionary item: {media}")   # Debugging
+       except AttributeError as e:
+            print(f"Unexpected Error occurred: {e}")
+            continue
+
+    return {"filtered results": filtered_data} 

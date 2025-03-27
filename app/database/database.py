8e7eafe1cca76d4b5536f450a1f9fef52ed5be8e
Alejandro Barragan <57509594+Darkling-01@users.noreply.github.com>
2025-03-26 18:28:49 -0700

    Update database.py
    implementing async technique

--- app/database/database.py
+++ app/database/database.py
@@ -1,15 +1,45 @@
 import sqlalchemy
-from sqlalchemy.ext.delcarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 DATABASE_URL = "postgresql+asyncpg://darkling:Gamer0077Dark@localhost/movie_API"
 
-# Async database connection
-database = Database(DATABASE_URL)
+# Async engine for query operations
+async_engine = create_async_engine(DATABASE_URL, echo=True)
 
-# Sync database connection (for non-async usage)
-engine = sqlachemy.create_engine(DATABASE_URL)
-session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Sync engine for table creation
+sync_engine = create_engine(DATABASE_URL, echo=True)
 
-base = declarative_base()
+# Session maker for async operations
+async_session = sessionmaker(
+                  bind=async_engine, class_=AsyncSession, expire_on_commit=False)
+
+# Creates base clss which SQLAlchemy models will inherit
+Base = declarative_base()
+
+class UserPreference(Base):
+    __tablename__  = "user_preference"
+
+    id = Column(Integer, primary_key=True, index=True)
+    media_type = Column(String, nullable=True)
+    actor_name = Column(String, nullable=True)
+    original_language = Column(String, nullable=True)
+    known_for_department = Column(String, nullable=True)
+
+
+# Dependency to get session
+#async def get_session():
+#    async with AsyncSessionLocal() as session:
+#        yield session
+
+# Dependency for getting the async session
+#def get_session():
+#    return async_session()
+
+def create_tables():
+    # Use sync engine only to create tables
+    Base.metadata.create_all(bind=sync_engine)
 
